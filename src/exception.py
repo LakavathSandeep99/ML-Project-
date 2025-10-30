505affd478b0e33773b7fc31f903d038631e06e7
LakavathSandeep99 <lakavathsandeep31@gmail.com>
2025-10-30 10:39:37 +0530

    execption and logger

--- src/exception.py
+++ src/exception.py
@@ -1,35 +1,25 @@
 import sys
-import logging
+from src.logger import logging
 
 
-def error_message_detail(error: Exception, exc_info=None) -> str:
-    """Return a concise message with filename and line number for the given exception.
-
-    If exc_info is None, sys.exc_info() is used to obtain the current exception info.
-    """
-    if exc_info is None:
-        exc_info = sys.exc_info()
-
-    _, _, tb = exc_info
-    # Walk to the last traceback frame (the origin of the exception)
-    while tb and tb.tb_next:
-        tb = tb.tb_next
-
-    if tb is None:
-        # Fallback if traceback is not available
-        return f"Error message [{error}]"
-
-    file_name = tb.tb_frame.f_code.co_filename
-    line_number = tb.tb_lineno
-    return f"Error occurred in python script name [{file_name}] line number [{line_number}] error message [{error}]"
-
+def error_massage_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = f"Error occured in python script name [{0}] line number [{1}] error message [{2}]".format()
+    file_name, exc_tb.tb_lineno, str(error)
+    return error_message
 
 class CustomException(Exception):
-    def __init__(self, error_message: Exception, exc_info=None):
+    def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, exc_info)
+        self.error_message = error_massage_detail(error_message, error_detail)
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.error_message
-
-
+    
+if __name__ == "__main__":
+    try:
+        a = 1 / 0
+    except Exception as e:
+        logging.info("Divide by Zerso")
+        raise CustomException(e, sys)
